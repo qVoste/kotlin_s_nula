00550fd083c4540e849ebb9efbceaebb80ddc0e3
qvoste <qvoste@gmail.com>
2025-02-25 20:00:16 +0700

    Цикл for

---
+++ src/main/kotlin/session_4/Main.kt
@@ -0,0 +1,11 @@
+package session_4
+
+fun main() {
+    var zxc = 1000
+    for (i in 1..143){
+        print(zxc)
+        zxc = zxc-7
+        println(" - 7 = $zxc")
+    }
+    print("я жигуль....")
+}
