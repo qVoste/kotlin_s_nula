4ce6b5da0929a9b34948c85aafe93900676ea04c
qvoste <qvoste@gmail.com>
2025-02-25 20:12:21 +0700

    Цикл while

---
+++ src/main/kotlin/session_5/Main.kt
@@ -0,0 +1,11 @@
+package session_5
+
+fun main() {
+    var zxc = 1000
+    while (zxc >= 0){
+        print("$zxc - 7 = ")
+        zxc = zxc - 7
+        println(zxc)
+    }
+    println("Я жигуль...")
+}
